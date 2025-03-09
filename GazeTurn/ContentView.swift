53c3a3c3277033a2e795d6ed80eca0ba38bccb1f
Jhen Mu <terminal201467@gmail.com>
2025-03-09 17:33:46 +0800

    Initial Commit

---
+++ GazeTurn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GazeTurn
+//
+//  Created by Jhen Mu on 2025/3/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GazeTurn/GazeTurnApp.swift
@@ -0,0 +1,17 @@
+//
+//  GazeTurnApp.swift
+//  GazeTurn
+//
+//  Created by Jhen Mu on 2025/3/9.
+//
+
+import SwiftUI
+
+@main
+struct GazeTurnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
